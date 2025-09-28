030ceb83149553e1b309fe609aee2f34ebddcd9d
sadko <kristiankastillo0@gmail.com>
2025-09-28 16:25:24 -0400

    feat(logger): Add configurable log level initialization

--- src/logger/mod.rs
+++ src/logger/mod.rs
@@ -1,1 +1,14 @@
+use env_logger::{Builder, Target};
+use log::LevelFilter;
 
+pub fn init(debug_mode: bool) {
+    let mut builder = Builder::from_default_env();
+
+    if debug_mode {
+        builder.filter_level(LevelFilter::Debug);
+    } else {
+        builder.filter_level(LevelFilter::Info);
+    }
+
+    builder.target(Target::Stdout).init();
+}
